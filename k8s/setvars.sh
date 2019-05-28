c7aa3dc45dcf5e0473873cba4a99a39dc442b46e
oceanosis <dumluufuk@gmail.com>
2019-05-28 15:41:57 +0100

    initial kubernetes deployment

---
+++ k8s/setvars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+unset ANSIBLE_CONFIG
+unset EC2_INI_PATH
+
+export BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export EC2_REGION="eu-west-2"
+export ANSIBLE_INVENTORY="$BASEDIR/ec2_ini.py"
+export ANSIBLE_ROLES_PATH="$BASEDIR/roles"
+export ANSIBLE_SSH_ARGS="-C -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -o ControlMaster=auto -o ControlPersist=60s"
+export ANSIBLE_PRIVATE_KEY_FILE="$BASEDIR/../aws/automation"
+
